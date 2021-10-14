6bd5927b9e4c1bffb1ebab9a2f280ff84f9c6c80
sophiaalthammer <sophia.althammer@gmail.com>
2021-10-14 17:36:01 +0200

    starts elasticsearch connection in utils/helpers.py, within engine/view.py the search function from elasticsearch client is called which returns a list of top_k retrieved documents (containing id and their text content)

--- src/dossier_search/engine/views.py
+++ src/dossier_search/engine/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 import simplejson as json
 from django.utils import timezone
 from flask import Markup
+from utils.helpers import search
 
 # Create your views here.
 
@@ -15,6 +16,10 @@ def index(request):
 
     """
     if request.method == "GET":
+        query = 'The appellant on February 9, 1961 was appointed as an Officer in Grade III in the respondent Bank ( for short).'
+        index = 'whole_doc_w_summ_intro'
+        top_k = 1
+        candidate_list = search(query, index, top_k)
         return render(
             request,
             "interfaces/welcome.html",
