684233fc2f46ec39c8e98451c062d8effdcca442
stamatisvas <vasilis.g.stamatis@gmail.com>
2022-03-22 15:39:36 +0200

    feat: refactor the mockup search engine feature

--- src/dossier_search/engine/search_engine_mockup.py
+++ src/dossier_search/engine/search_engine_mockup.py
@@ -1,24 +1,15 @@
+import json
+
 path_to_file = '/home/fps/Downloads/AMiner_sample_l10.jsonl'
 
-with open(path_to_file, 'r', encoding='utf-8') as reader:
-    for line in reader:
-        print(line)
-        break
 
-import json
+def search_engine_mockup():
+    with open(path_to_file, 'r') as json_file:
+        json_list = list(json_file)
 
-data = json.load(path_to_file)
-
-import json
-
-with open(path_to_file, 'r') as json_file:
-    json_list = list(json_file)
-
-for json_str in json_list:
-    result = json.loads(json_str)
-    print(f"result: {result}")
-    print(isinstance(result, dict))
-    break
-
-result['docno']
-result['title']
+    results_list = []
+    for json_str in json_list:
+        result = json.loads(json_str)
+        results_list.append(result)
+        print(f"result: {result}")
+    return results_list
