dda557018cb2c4751dec28ea311aa8e5a36e0892
Wojciech Kusa <WojciechKusa@users.noreply.github.com>
2022-03-22 17:50:52 +0100

    feat(data): add article and wikipedia data schema (#16)

---
+++ src/dossier_search/utils/article.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Article:
+    """Class for representing article from AMiner."""
+    id: str
+    title: str
+    url: str
+    snippet: str
+    abstract: str
+    authors: str
+    publication_date: str = ""
+    venue: str = ""
+
+
+@dataclass()
+class WikipediaArticle:
+    """Class for representing Wikipedia article."""
+    id: str
+    title: str
+    url: str
+    snippet: str
+    content: str
