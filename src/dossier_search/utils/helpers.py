2b6eb53f86c30abb09b8da3b7523327bf094e869
Tobias Fink <tobiasfink.ai@gmail.com>
2022-03-22 18:33:09 +0100

    feat: Added Article format to search

--- src/dossier_search/utils/helpers.py
+++ src/dossier_search/utils/helpers.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from utils.article import Article
 
 
 def search(query: str, index: str, top_k: int):
@@ -10,8 +11,33 @@ def search(query: str, index: str, top_k: int):
     results = res.json()['results']
     candidate_list = []
     for candidate in results["hits"]["hits"]:
-        candidate_list.append(
-            {"id": candidate["_id"], "content": candidate["_source"].get("document")}
+        doc_text = candidate["_source"].get("document")
+        if doc_text:
+            snippet = doc_text[:160]
+        else:
+            snippet = ""
+        authors_raw = candidate["_source"].get("authors")
+        if authors_raw:
+            author_details = [author["name"] for author in authors_raw]
+        else:
+            author_details = []
+        
+        venue_raw = candidate["_source"].get("venue")
+        if venue_raw:
+            venue = venue_raw.get("raw")
+        else:
+            venue = ""
+
+        retrieved_art = Article(
+            id=candidate["_id"],
+            title=candidate["_source"].get("title"),
+            url=candidate["_source"].get("url"),
+            snippet=snippet,
+            abstract=candidate["_source"].get("document"),
+            authors=", ".join(author_details),
+            publication_date="publication_date",
+            venue=venue
         )
+        candidate_list.append(retrieved_art)
 
     return candidate_list
