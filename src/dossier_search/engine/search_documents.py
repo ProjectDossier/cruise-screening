62e868e372c2f73cfd4ba1ecb82f3b81b9aa45a3
aminvenv <aminvenv@gmail.com>
2022-03-23 15:21:28 +0100

    fix: balanced highlight tag for snippet

--- src/dossier_search/engine/search_documents.py
+++ src/dossier_search/engine/search_documents.py
@@ -13,7 +13,13 @@ def highlighter(query: str, doc:str):
                 break
         sentence_pool.append(sentence)
     highlighted_text = '.'.join(sentence_pool)
-    return highlighted_text
+
+    snippet = ""
+    for sentence in sentence_pool:
+        if len(snippet)<200:
+            snippet += sentence + '.'
+
+    return highlighted_text, snippet
 
 def search(query: str, index: str, top_k: int):
     headers = {
@@ -25,8 +31,7 @@ def search(query: str, index: str, top_k: int):
     for candidate in results["hits"]["hits"]:
         doc_text = candidate["_source"].get("document")
         if doc_text:
-            abstract = highlighter(query, doc_text)
-            snippet = abstract[:160] # doc_text[:160]
+            abstract, snippet = highlighter(query, doc_text)
         else:
             abstract = ""
             snippet = ""
