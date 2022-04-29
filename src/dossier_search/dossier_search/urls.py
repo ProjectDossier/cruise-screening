02b543d466d410cbb5afaca8c92b9d447a49b07e
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-04-29 16:41:19 +0200

    refactor: refactor views and urls

--- src/dossier_search/dossier_search/urls.py
+++ src/dossier_search/dossier_search/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from engine import views
+from django.urls import path, include
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("admin/", admin.site.urls),
-    path("home/", views.index, name="home"),
-    path("about/", views.about, name="about"),
-    path("search", views.search_results, name="search_results"),
+    path("", include("engine.urls")),
 ]
