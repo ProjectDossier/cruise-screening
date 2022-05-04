6f0ef3dcda1538f9eca2d208bcd396e602bc4712
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-05-04 16:33:22 +0200

    fix(users): implement navbar for users

--- src/dossier_search/users/urls.py
+++ src/dossier_search/users/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("register/", views.register, name="register"),
-    path("logout", views.logout_request, name="logout"),
+    path("logout/", views.logout_request, name="logout"),
     path("login/", views.login_request, name="login"),
 ]
