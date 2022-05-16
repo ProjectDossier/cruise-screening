61dc4ec417d271ef33746d6b3de15b80e5f07e48
Wojciech Kusa <WojciechKusa@users.noreply.github.com>
2022-05-16 18:33:52 +0200

    Implement user accounts (#79)
    * refactor: refactor views and urls
    
    * feat(users): add users django app with stub for user login and registration
    
    * fix(users): implement navbar for users
    
    * feat(users): add user profile page
    
    * feat(users): add custom fields to the user profile
    
    * fix: rewove duplicated search box in search_result.html
    
    * fix: remove terms of service text

--- src/dossier_search/dossier_search/settings.py
+++ src/dossier_search/dossier_search/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "engine",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.User'
--- src/dossier_search/dossier_search/urls.py
+++ src/dossier_search/dossier_search/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("engine.urls")),
+    path("", include("users.urls")),
 ]
