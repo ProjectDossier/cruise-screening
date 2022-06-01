b8428bd9b34b0920ef73eea901b83a5117ad4fdc
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-06-02 00:42:27 +0200

    revert settings.py

--- src/dossier_search/dossier_search/settings.py
+++ src/dossier_search/dossier_search/settings.py
@@ -141,6 +141,6 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # change to True if you are running this code on M1 Apple Macbook
-M1_CHIP = True
+M1_CHIP = False
 
 AUTH_USER_MODEL = 'users.User'
