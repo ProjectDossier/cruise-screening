0bdad928f2b65590fb294f2ae81c2d7a3b0e3939
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-06-30 12:13:44 +0200

    feat(taxonomy): taxonomy search separated from the document search

--- src/cruise_literature/concept_search/urls.py
+++ src/cruise_literature/concept_search/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
+    path(r"search_concepts/<query>/", views.search_concepts, name="search_concepts"),
 ]
--- src/cruise_literature/concept_search/views.py
+++ src/cruise_literature/concept_search/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from src.cruise_literature.concept_search.taxonomy import TaxonomyRDFCSO, TaxonomyRDFCCS
+
+taxonomies = {
+    "CSO": TaxonomyRDFCSO("../../data/external/"),
+    "CCS": TaxonomyRDFCCS("../../data/external/"),
+    "Wikipedia": TaxonomyRDFCCS(
+        "../../data/external/",
+        filename="wikipedia_taxonomy.xml",
+        taxonomy_name="wikipedia",
+    ),
+}
+
+
+def search_concepts(request, query):
+    """
+    Search concepts relevant to the query inside taxonomies
+    """
+    if request.method == "GET":
+        tax_results = []
+        for name, taxonomy in taxonomies.items():
+            concept = taxonomy.search(query=query)
+            tax_results.append({
+                "name": name,
+                "concept": concept.to_dict(),
+                "subparents": [
+                    item.to_dict()
+                    for sublist in concept.parents
+                    for item in sublist.parents
+                ],
+                "subchildren": [
+                    item.to_dict()
+                    for sublist in concept.children
+                    for item in sublist.children
+                ],
+                "parents": [x.to_dict() for x in concept.parents],
+                "children": [x.to_dict() for x in concept.children],
+            }
+            )
+
+        return JsonResponse(tax_results, safe=False)
