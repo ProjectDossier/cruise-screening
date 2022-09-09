f03312d56fc81ff3805a4f16d0c41e8b96df69a5
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-09-09 16:15:28 +0200

    feat(SemanticScholar search): add function for searching with S2 API

---
+++ src/cruise_literature/document_search/search_semantic_scholar.py
@@ -0,0 +1,80 @@
+from typing import List
+import requests
+from utils.article import Article, Author
+
+from concept_search.concept_classification import CSOClassification
+
+API_ENDPOINT = "https://api.semanticscholar.org/graph/v1/paper/search?query="
+
+FIELDS = "externalIds,url,title,abstract,venue,year,referenceCount,citationCount,influentialCitationCount,isOpenAccess,\
+fieldsOfStudy,s2FieldsOfStudy,publicationTypes,publicationDate,journal,authors"
+
+cso_cls = CSOClassification()
+
+
+def get_authors(authors_list):
+    _authors = []
+    for _author in authors_list:
+        _authors.append(
+            Author(
+                display_name=_author.get("name"),
+                semantic_scholar_id=_author.get("authorId"),
+            )
+        )
+
+    return _authors
+
+
+def search_semantic_scholar(query: str, index: str, top_k: int) -> List[Article]:
+    response = requests.get(
+        f"{API_ENDPOINT}{'+'.join(query.split())}&limit={top_k}&fields={FIELDS}"
+    )
+    # http://api.semanticscholar.org/graph/v1/paper/search?query=literature+graph&offset=10&limit=50&fields=title,authors
+
+    candidate_list = []
+    if response.status_code == 200:
+        for index_i, candidate in enumerate(response.json()["data"]):
+            try:
+                snippet = candidate.get("abstract")[:300]
+                abstract = candidate.get("abstract")
+            except TypeError:
+                snippet = ""
+                abstract = ""
+
+            if candidate.get("externalIds").get("ArXiv"):
+                pdf = f"https://arxiv.org/pdf/{candidate.get('externalIds').get('ArXiv')}.pdf"
+            else:
+                pdf = None
+
+            authors = get_authors(candidate.get("authors"))
+            authors = ", ".join([a.display_name for a in authors])
+            date = candidate["publicationDate"]
+            if date:
+                year = date[:4]
+            else:
+                year = "   "
+
+            retrieved_art = Article(
+                id=candidate["paperId"],
+                # semantic_scholar_id=None,
+                # core_id=None,
+                title=candidate.get("title"),
+                url=candidate["url"],
+                pdf=pdf,
+                snippet=snippet,
+                abstract=abstract,
+                authors=authors,
+                publication_date=year,
+                venue=candidate["venue"],
+                keywords_snippet=None,  # keywords_snippet,
+                keywords_rest=None,  # keywords_rest,
+                CSO_keywords=None,  # candidate["_source"].get("CSO_keywords")['union'],
+                citations=candidate.get("citationCount"),
+                references=candidate.get("referenceCount"),
+            )
+            if index_i < 10:
+                retrieved_art = cso_cls.classify_single_paper(retrieved_art)
+            candidate_list.append(retrieved_art)
+
+    # candidate_list = cso_cls.classify_search_result(candidate_list[:10])
+    return candidate_list
