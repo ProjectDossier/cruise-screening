aa49890ff36424e7984dbda10be0be3775a2d204
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-09-11 18:06:36 +0200

    refactor(citation screening): refactor screen_papers()

--- src/cruise_literature/citation_screening/views.py
+++ src/cruise_literature/citation_screening/views.py
@@ -75,64 +75,63 @@ def screen_papers(request, review_id):
         return redirect("home")
 
     review = get_object_or_404(LiteratureReview, pk=review_id)
-    if request.user in review.members.all():
-        if request.method == "GET":
-            for paper in review.papers:
-                if not paper.get("decisions") or len(paper.get("decisions")) < MIN_DECISIONS:
-                    return render(
-                        request,
-                        "literature_review/screen_paper.html",
-                        {"review": review, "paper": paper},
-                    )
-        elif request.method == "POST":
-            keys = request.POST.keys()
-            exclusions = [request.POST[x] for x in keys if x.startswith("exclusion")]
-            inclusions = [request.POST[x] for x in keys if x.startswith("inclusion")]
-
-            paper_id = request.POST["paper_id"]
-            reason = request.POST["reason"]
-            eligibility_decision = make_decision(
-                exclusions=exclusions, inclusions=inclusions
-            )
-            topic_relevance = request.POST["topic_relevance"]
-            domain_relevance = request.POST["domain_relevance"]
-            decision = request.POST["decision"]
-            prior_knowledge = request.POST["prior_knowledge"]
-
-            edited_index = [
-                index_i
-                for index_i, x in enumerate(review.papers)
-                if x["id"] == paper_id
-            ][0]
-            review.papers[edited_index]["decisions"] = [
-                {
-                    "reviewer_id": request.user.pk,
-                    "decision": decision,
-                    "eligibility_decision": eligibility_decision,
-                    "reason": reason,
-                    "inclusions": inclusions,
-                    "exclusions": exclusions,
-                    "stage": "title_abstract",
-                    "domain_relevance": int(domain_relevance),
-                    "topic_relevance": int(topic_relevance),
-                    "prior_knowledge": int(prior_knowledge),
-                }
-            ]
-            review.papers[edited_index]["decision"] = decision
-            if len(review.papers[edited_index]["decisions"]) >= MIN_DECISIONS:
-                review.papers[edited_index]["screened"] = True
-            review.save()
-
-            for paper in review.papers:
-                if not paper.get("decisions") or len(paper.get("decisions")) < MIN_DECISIONS:
-                    return render(
-                        request,
-                        "literature_review/screen_paper.html",
-                        {"review": review, "paper": paper},
-                    )
-
-    else:
+    if request.user not in review.members.all():
         return redirect("home")
+
+    if request.method == "GET":
+        for paper in review.papers:
+            if not paper.get("decisions") or len(paper.get("decisions")) < MIN_DECISIONS:
+                return render(
+                    request,
+                    "literature_review/screen_paper.html",
+                    {"review": review, "paper": paper},
+                )
+    elif request.method == "POST":
+        keys = request.POST.keys()
+        exclusions = [request.POST[x] for x in keys if x.startswith("exclusion")]
+        inclusions = [request.POST[x] for x in keys if x.startswith("inclusion")]
+
+        paper_id = request.POST["paper_id"]
+        reason = request.POST["reason"]
+        eligibility_decision = make_decision(
+            exclusions=exclusions, inclusions=inclusions
+        )
+        topic_relevance = request.POST["topic_relevance"]
+        domain_relevance = request.POST["domain_relevance"]
+        decision = request.POST["decision"]
+        prior_knowledge = request.POST["prior_knowledge"]
+
+        edited_index = [
+            index_i
+            for index_i, x in enumerate(review.papers)
+            if x["id"] == paper_id
+        ][0]  # FIXME: this will fail if duplicates in DB
+        review.papers[edited_index]["decisions"] = [
+            {
+                "reviewer_id": request.user.pk,
+                "decision": decision,
+                "eligibility_decision": eligibility_decision,
+                "reason": reason,
+                "inclusions": inclusions,
+                "exclusions": exclusions,
+                "stage": "title_abstract",
+                "domain_relevance": int(domain_relevance),
+                "topic_relevance": int(topic_relevance),
+                "prior_knowledge": int(prior_knowledge),
+            }
+        ]
+        review.papers[edited_index]["decision"] = decision
+        if len(review.papers[edited_index]["decisions"]) >= MIN_DECISIONS:
+            review.papers[edited_index]["screened"] = True
+        review.save()
+
+        for paper in review.papers:
+            if not paper.get("decisions") or len(paper.get("decisions")) < MIN_DECISIONS:
+                return render(
+                    request,
+                    "literature_review/screen_paper.html",
+                    {"review": review, "paper": paper},
+                )
 
 
 def export_review(request, review_id):
