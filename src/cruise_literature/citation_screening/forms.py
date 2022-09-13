8617cd1bdf98851ac51d82e9edde17c85e641de2
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-09-13 12:53:37 +0200

    feat(literature review): add option for selecting search engines while creating new review

--- src/cruise_literature/citation_screening/forms.py
+++ src/cruise_literature/citation_screening/forms.py
@@ -11,7 +11,14 @@ from django.contrib.postgres.forms import (
 )
 from document_search.search_semantic_scholar import search_semantic_scholar
 from document_search.search_core import search_core
+from document_search.search_documents import search
 from users.models import KnowledgeArea
+
+SEARCH_ENGINES_DICT = {
+    "SemanticScholar": search_semantic_scholar,
+    "CORE": search_core,
+    "CRUISE": search,
+}
 
 
 class ArrayFieldStripWhitespaces(SimpleArrayField):
@@ -88,6 +95,12 @@ class NewLiteratureReviewForm(forms.ModelForm):
     annotations_per_paper = forms.ChoiceField(
         choices=[(1, 1), (2, 2), (3, 3)], widget=forms.Select(attrs={"class": "select"})
     )
+    search_engines = forms.MultipleChoiceField(
+        label="Select search engines where you want to search for papers. By default it searches in all",
+        choices=[(k, ' '.join(k.split('_'))) for k in SEARCH_ENGINES_DICT.keys()],
+        initial=list(SEARCH_ENGINES_DICT.keys()),
+        widget=forms.SelectMultiple(attrs={"class": "select is-multiple is-medium"}),
+    )
     top_k = forms.IntegerField(
         initial=25,
         max_value=200,
@@ -115,19 +128,18 @@ class NewLiteratureReviewForm(forms.ModelForm):
         super(NewLiteratureReviewForm, self).__init__(*args, **kwargs)
 
     def save(self, commit=True):
+        INDEX_NAME = 'papers'  # TODO: get rid of this parameter
         instance = super(NewLiteratureReviewForm, self).save(commit=False)
         top_k = self.data.get("top_k")
+        search_engines = self.cleaned_data["search_engines"]
 
         queries = self.cleaned_data["search_queries"]
         results = {}
         for query in queries:
             # TODO: add more search engines
-            search_engines = {
-                "semantic_scholar": search_semantic_scholar,
-                "CORE": search_core,
-            }
-            for search_engine_name, search_method in search_engines.items():
-                for paper in search_method(query=query, index="", top_k=top_k):
+            for search_engine_name in search_engines:
+                search_method = SEARCH_ENGINES_DICT[search_engine_name]
+                for paper in search_method(query=query, index=INDEX_NAME, top_k=top_k):
                     paper = asdict(paper)
                     paper["query"] = query
                     paper["search_engine"] = search_engine_name
