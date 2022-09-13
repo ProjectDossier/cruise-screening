d0b3e1a39f7b968c1d9959f769256e074a3eb9b6
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-09-13 17:08:28 +0200

    feat(automatic classification): add views and serializers for ml algorithms

--- src/cruise_literature/document_classification/views.py
+++ src/cruise_literature/document_classification/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.db import transaction
+from rest_framework import viewsets, mixins
+from rest_framework.exceptions import APIException
 
-# Create your views here.
+from .models import Endpoint
+from .models import MLAlgorithm
+from .models import MLAlgorithmStatus
+from .models import MLRequest
+from .serializers import EndpointSerializer
+from .serializers import MLAlgorithmSerializer
+from .serializers import MLAlgorithmStatusSerializer
+from .serializers import MLRequestSerializer
+
+
+class EndpointViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
+    serializer_class = EndpointSerializer
+    queryset = Endpoint.objects.all()
+
+
+class MLAlgorithmViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
+    serializer_class = MLAlgorithmSerializer
+    queryset = MLAlgorithm.objects.all()
+
+
+def deactivate_other_statuses(instance):
+    old_statuses = MLAlgorithmStatus.objects.filter(
+        parent_mlalgorithm=instance.parent_mlalgorithm,
+        created_at__lt=instance.created_at,
+        active=True,
+    )
+    for i in range(len(old_statuses)):
+        old_statuses[i].active = False
+    MLAlgorithmStatus.objects.bulk_update(old_statuses, ["active"])
+
+
+class MLAlgorithmStatusViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+    mixins.CreateModelMixin,
+):
+    serializer_class = MLAlgorithmStatusSerializer
+    queryset = MLAlgorithmStatus.objects.all()
+
+    def perform_create(self, serializer):
+        try:
+            with transaction.atomic():
+                instance = serializer.save(active=True)
+                # set active=False for other statuses
+                deactivate_other_statuses(instance)
+
+        except Exception as e:
+            raise APIException(str(e)) from e
+
+
+class MLRequestViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+    mixins.UpdateModelMixin,
+):
+    serializer_class = MLRequestSerializer
+    queryset = MLRequest.objects.all()
