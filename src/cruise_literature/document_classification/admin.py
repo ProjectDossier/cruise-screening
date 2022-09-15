7964a0b574b25909c85f5e7fdf60a89e46e3eb73
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-09-15 19:09:57 +0200

    feat(automatic classification): add document_classification models to django admin

--- src/cruise_literature/document_classification/admin.py
+++ src/cruise_literature/document_classification/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Endpoint, MLAlgorithm, MLAlgorithmStatus, MLRequest
+
+admin.site.register(Endpoint)
+admin.site.register(MLAlgorithm)
+admin.site.register(MLAlgorithmStatus)
+admin.site.register(MLRequest)
