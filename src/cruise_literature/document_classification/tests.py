23da809715833384d237d48af8b96804b3eccac5
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-09-15 14:53:15 +0200

    test(automatic classification): add classifier and registry tests

--- src/cruise_literature/document_classification/tests.py
+++ src/cruise_literature/document_classification/tests.py
@@ -1,3 +1,37 @@
+import inspect
+
 from django.test import TestCase
 
-# Create your tests here.
+from .classifiers.dummy import DummyClassifier
+from .registry import MLRegistry
+from users.models import User
+
+
+class DocumentClassificationTests(TestCase):
+    def test_registry(self):
+        user = User.objects.create_user("myuser", "myemail@test.com", "test_password")
+        registry = MLRegistry()
+        self.assertEqual(len(registry.endpoints), 0)
+        endpoint_name = "text_classification"
+        algorithm_object = DummyClassifier()
+        algorithm_name = "dummy classifier"
+        algorithm_status = "production"
+        algorithm_version = "0.0.1"
+        algorithm_owner = user  # TODO: replace with user
+        algorithm_description = "Dummy classifier always predicting '1'."
+        algorithm_code = inspect.getsource(DummyClassifier)
+
+        # add to registry
+        registry.add_algorithm(
+            endpoint_name,
+            algorithm_object,
+            algorithm_name,
+            algorithm_status,
+            algorithm_version,
+            algorithm_owner,
+            algorithm_description,
+            algorithm_code,
+        )
+
+        # there should be one endpoint available
+        self.assertEqual(len(registry.endpoints), 1)
