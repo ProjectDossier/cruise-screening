e6b529a7376accc5bc606b72b0359ad80764e2bc
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-11-25 21:54:57 +0100

    fix(users): add default django login URL

--- src/cruise_literature/users/urls.py
+++ src/cruise_literature/users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("logout/", views.logout_request, name="logout"),
     path("login/", views.login_request, name="login"),
+    path("accounts/login/", views.login_request, name="login"),
     path("profile", views.user_profile, name="user_profile"),
     path("edit_user_profile", views.edit_profile, name="edit_user_profile"),
 ]
