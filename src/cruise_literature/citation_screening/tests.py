afa8133a43916b47160bf7389912a389bac907b2
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-11-25 22:22:12 +0100

    test(literature review): add unit tests for review details

--- src/cruise_literature/citation_screening/tests.py
+++ src/cruise_literature/citation_screening/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 from users.models import User
+from .models import LiteratureReview, LiteratureReviewMember
 
 from .views import (
     create_new_review,
@@ -31,7 +32,6 @@ class ViewTests(TestCase):
             automatic_screening,
             add_seed_studies,
         ]
-
         cls.user = User.objects.create_user(
             username="testuser",
             email="",
@@ -39,12 +39,23 @@ class ViewTests(TestCase):
         cls.user.set_password("testpassword")
         cls.user.save()
 
+        cls.lit_rev = LiteratureReview.objects.create(
+            title="Test Literature Review",
+            description="Test Description",
+            project_deadline="2020-01-01",
+        )
+        cls.member = LiteratureReviewMember.objects.create(
+            member=cls.user, literature_review=cls.lit_rev
+        )
+
     def setUp(self):
+        self.client = Client()
         self.client.login(username="testuser", password="testpassword")
 
         self.literature_review_list_url = reverse(
             "literature_review:literature_review_home"
         )
+        self.review_details_url = reverse("literature_review:review_details", args=[1])
 
     def test_literature_review_list_GET(self):
         response = self.client.get(self.literature_review_list_url)
@@ -60,7 +71,33 @@ class ViewTests(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, "/accounts/login/?next=/literature_review/")
 
-    def test_review_details(self):
-        review_details_url = "/literature_review/1/"
-        response = self.client.get(review_details_url)
+    def test_review_details_GET(self):
+        response = self.client.get(self.review_details_url)
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "literature_review/view_review.html")
+        self.assertTemplateUsed(response, "literature_review/_review_header.html")
+        for template in base_templates:
+            self.assertTemplateUsed(response, template)
+
+    def test_review_details_GET_unauthenticated(self):
+        self.client.logout()
+        response = self.client.get(self.review_details_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/accounts/login/?next=/literature_review/1/")
+
+    def test_review_details_GET_not_member(self):
+        self.client.logout()
+        usr2 = User.objects.create_user(
+            username="testuser2",
+            email="",
+        )
+        usr2.set_password("testpassword")
+        usr2.save()
+        self.client.login(username="testuser2", password="testpassword")
+        response = self.client.get(self.review_details_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/")
+
+    def test_review_details_GET_not_exist(self):
+        response = self.client.get(reverse("literature_review:review_details", args=[2]))
+        self.assertEqual(response.status_code, 404)
