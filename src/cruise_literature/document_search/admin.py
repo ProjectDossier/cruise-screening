de651282b91495b20e7e0d79f2accf7048097153
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-11-26 19:01:21 +0100

    feat(document search): create a database model for search engines
    - add available search engines to the database

--- src/cruise_literature/document_search/admin.py
+++ src/cruise_literature/document_search/admin.py
@@ -1,1 +1,5 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import SearchEngine
+
+admin.site.register(SearchEngine)
--- src/cruise_literature/document_search/models.py
+++ src/cruise_literature/document_search/models.py
@@ -1,1 +1,23 @@
-# Create your models here.
+from django.db import models
+
+from django.utils.translation import gettext_lazy as _
+
+
+class SearchEngine(models.Model):
+    """Search engine that can be used to search for documents."""
+
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    url = models.URLField()
+
+    search_method = models.CharField(max_length=255, blank=True, null=True)
+
+    is_available = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
