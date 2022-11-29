5297f8b46c62045ffebbd39d7162759db781d943
Wojciech Kusa <wojciech.kusa@gmail.com>
2022-11-29 15:15:22 +0100

    fix(document classification): fix issues based on code review

--- src/cruise_literature/document_classification/tests.py
+++ src/cruise_literature/document_classification/tests.py
@@ -45,7 +45,4 @@ class EndpointTests(TestCase):
         input_data = ["This text should be included"]
         classifier_url = "/api/v1/text_classification/predict"
         response = client.post(classifier_url, input_data, format="json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["label"], "<=50K")
-        self.assertTrue("request_id" in response.data)
-        self.assertTrue("status" in response.data)
+        self.assertEqual(response.status_code, 404)
