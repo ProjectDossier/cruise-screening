8c3a3d9b3f35524cce8645c1b3576e5f13d4bce5
Michal Strzezon <mstrzezon@gmail.com>
2024-11-25 22:32:25 +0100

    feat(backend): refactor code

--- src/backend/ml_api/flaskr/classifiers/binary/fasttext_classifier.py
+++ src/backend/ml_api/flaskr/classifiers/binary/fasttext_classifier.py
@@ -42,12 +42,10 @@ class FastTextClassifier(BaseClassifier):
 
     def train(self, input_data: List[str], true_labels: List[int]) -> None:
         input_data = self.preprocessing(input_data=input_data)
-        print("Preprocessing done")
         write_temp_fasttext_train_file(
             input_data=input_data, outfile=self.temp_file_path, labels=true_labels
         )
         self.model = fasttext.train_supervised(input=self.temp_file_path, epoch=70)
-        print("Model trained")
         delete_temp_fasttext_train_file(self.temp_file_path)
 
     def predict(self, input_data: List[str]) -> List[Tuple[int, float]]:
