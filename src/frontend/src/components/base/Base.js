99742ea3fd274aae2ddb05ed888194c1fda64489
paulinagacek <paulina.gacek.pl@gmail.com>
2024-12-03 08:45:13 +0100

    Fixes in Base, Home and Footer

--- src/frontend/src/components/base/Base.js
+++ src/frontend/src/components/base/Base.js
@@ -1,33 +1,33 @@
 import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 
 function Base({ title = 'Cruise-literature', children }) {
     return (
-        <html lang="en">
-        <link rel="stylesheet" href="style.css" />
-            <head>
-                <meta charSet="utf-8" />
-                <title>{title}</title>
-                <meta name="description" content="" />
-                <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link
-                    rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
-                />
-                <link rel="stylesheet" type="text/css" href="/static/css/style.css" />
-                <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-                <script src="sort-paginate-table.js"></script>
-                <script src="modal.js"></script>
-            </head>
-            <body>
+        <HelmetProvider>
+            <>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>{title}</title>
+                    <meta name="description" content="" />
+                    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link
+                        rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
+                    />
+                    <link rel="stylesheet" type="text/css" href="/static/css/style.css" />
+                    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+                    <script src="sort-paginate-table.js"></script>
+                    <script src="modal.js"></script>
+                </Helmet>
                 <Header />
                 {children}
                 <Footer />
-            </body>
-        </html>
+            </>
+        </HelmetProvider>
     );
-};
+}
 
 export default Base;
--- src/frontend/src/components/home/Home.js
+++ src/frontend/src/components/home/Home.js
@@ -4,15 +4,14 @@ import Base from '../base/Base'
 function Home({ searchQuery }) {
     return (
         <Base>
-            <link rel="stylesheet" href="style.css" />
-            <div class="search-box">
-                <img src="cruise-logo.png" class="search-box__logo" width="300" height="300" />
+            <div className="search-box">
+                <img src="cruise-logo.png" className="search-box__logo" width="300" height="300" />
 
                 <form id="search_box" method="" action="">{/* TODO */}
-                    <div class="search-box__input">
-                        <input type="search" class="input mr-2" name="search_query" value={searchQuery}
+                    <div className="search-box__input">
+                        <input type="search" className="input mr-2" name="search_query" value={searchQuery}
                             placeholder="Search CRUISE" aria-label="Search" />
-                        <button class="button is-primary">Search</button>
+                        <button className="button is-primary">Search</button>
                     </div>
                     <input type='hidden' name='source' value="main_search" />
                 </form>
